d92df0ee9add8033f468ea09a4d96a47d1207283
Christian Müller <dev@c-mueller.xyz>
2019-07-19 18:36:31 +0200

    Update to new Packagepath for caddy

--- setup.go
+++ setup.go
@@ -15,10 +15,10 @@
 package ads
 
 import (
+	"github.com/caddyserver/caddy"
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
 	"github.com/coredns/coredns/plugin/metrics"
-	"github.com/mholt/caddy"
 	"time"
 )
 
